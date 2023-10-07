2e8a58b7ef47d52c3ea81d4b36c50c248957bf0a
miroslav.mitrovic <mmitrovicmas13@gmail.com>
2023-10-07 10:22:00 +0200

    removal of GTest Framework build files

--- Testing/DynamicTests/build/build/_deps/googletest-src/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/atmel/attiny85/IgnitionController/unitTests/build/_deps/googletest-src/googletest
-# Build directory: D:/projects/atmel/attiny85/IgnitionController/unitTests/build/build/_deps/googletest-src/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
